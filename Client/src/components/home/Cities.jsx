cea134b05f180b012a8af7d652b8a1a1a1d9b566
thanh6886 <phanquangthanh136@gmail.com>
2024-02-25 01:58:16 +0700

    .

--- Client/src/components/home/Cities.jsx
+++ Client/src/components/home/Cities.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
-import styles from './Cities.module.css'
+import React from "react";
+import { useContext, useState } from "react";
+import SearchDataContext from "../../store/search-data-context";
+import styles from "./Cities.module.css";
+import { Link } from "react-router-dom";
 
 // show options cities
 function Cities({ propsByArea }) {
+  const { findHotels } = useContext(SearchDataContext);
   const cities = [
     {
-      name: 'Ha Noi',
+      name: "Ha Noi",
       props: propsByArea.HN,
-      image: './images/hanoi-3609871_1920.jpg',
+      image: "./images/hanoi-3609871_1920.jpg",
     },
     {
-      name: 'Ho Chi Minh',
+      name: "Ho Chi Minh",
       props: propsByArea.HCM,
-      image: './images/sai-gon.jpg',
+      image: "./images/sai-gon.jpg",
     },
     {
-      name: 'Da Nang',
+      name: "Da Nang",
       props: propsByArea.DN,
-      image: './images/da-anng.jpg',
+      image: "./images/da-anng.jpg",
     },
-  ]
+  ];
+
   return (
     <div className={styles.cities}>
-      {cities.map(city => (
+      {cities.map((city) => (
         <div className={styles.city} key={city.name}>
           <div className={styles.img}>
-            <img
-              src={city.image}
-              alt={city.name}
-            />
+            <img src={city.image} alt={city.name} />
           </div>
           <div className={styles.content}>
             <h1>{city.name}</h1>
@@ -37,7 +39,7 @@ function Cities({ propsByArea }) {
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Cities
+export default Cities;
--- Client/src/components/home/Types.jsx
+++ Client/src/components/home/Types.jsx
@@ -1,52 +1,61 @@
-import styles from './Types.module.css'
+import styles from "./Types.module.css";
 
 // show types hotels
-function Types({propsByType}) {
-	const types = [
-		{
-			"name": "Hotels",
-			"count": propsByType.hotel,
-			"image": "./images/type_1.webp"
-		},
-		{
-			"name": "Apartments",
-			"count": propsByType.apartment,
-			"image": "./images/type_2.jpg"
-		},
-		{
-			"name": "Resorts",
-			"count": propsByType.resort,
-			"image": "./images/type_3.jpg"
-		},
-		{
-			"name": "Villas",
-			"count": propsByType.villa,
-			"image": "./images/type_4.jpg"
-		},
-		{
-			"name": "Cabins",
-			"count": propsByType.cabin,
-			"image": "./images/type_5.jpg"
-		}
-	]
+function Types({ propsByType }) {
+  const types = [
+    {
+      name: "Hotels",
+      count: propsByType.hotel,
+      image: "./images/type_1.webp",
+    },
+    {
+      name: "Apartments",
+      count: propsByType.apartment,
+      image: "./images/type_2.jpg",
+    },
+    {
+      name: "Resorts",
+      count: propsByType.resort,
+      image: "./images/type_3.jpg",
+    },
+    {
+      name: "Villas",
+      count: propsByType.villa,
+      image: "./images/type_4.jpg",
+    },
+    {
+      name: "Cabins",
+      count: propsByType.cabin,
+      image: "./images/type_5.jpg",
+    },
+  ];
   return (
     <div className={styles.wrap}>
-	    <h2>Browse by property type</h2>
+      <h2>Browse by property type</h2>
       <div className={styles.types}>
         {types.map((type, index) => (
           <div className={styles.type} key={index}>
             <div className={styles.img}>
-              <img src={type.image} alt={type.name} width='100%' height='150px'/>
+              <img
+                src={type.image}
+                alt={type.name}
+                width="100%"
+                height="150px"
+              />
             </div>
-            <div className={styles.content}>
-              <h4>{type.name}</h4>
-              <p>{type.count} {type.name.toLowerCase()}</p>
+            <div className={styles.flexcontent}>
+              <div className={styles.content}>
+                <h4>{type.name}</h4>
+                <p>
+                  {type.count} {type.name.toLowerCase()}
+                </p>
+              </div>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Types
+export default Types;
